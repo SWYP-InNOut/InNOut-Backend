18d32261a981ef9ef7f9a68bd01d0926c1910189
정소민 <yjk70702002@gmail.com>
2024-07-05 14:32:52 +0900

    Feat: health checking

---
+++ src/main/java/inandout/backend/healthcheck/HealthCheckController.java
@@ -0,0 +1,16 @@
+package inandout.backend.healthcheck;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class HealthCheckController {
+    @GetMapping("/healthcheck")
+    public String testConnection() {
+        return "ok";
+    }
+}
