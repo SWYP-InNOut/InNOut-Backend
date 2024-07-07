3a1b2786b176f72d1fddd322096b7688cd4016a1
dainshon <81453127+dainshon@users.noreply.github.com>
2024-07-07 20:10:59 +0900

    Test: CICD 테스트

--- src/main/java/inandout/backend/healthcheck/HealthCheckController.java
+++ src/main/java/inandout/backend/healthcheck/HealthCheckController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
     @GetMapping("/healthcheck")
     public String testConnection() {
-        return "ok";
+        return "CICD ok";
     }
 }
