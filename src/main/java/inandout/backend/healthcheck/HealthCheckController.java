7b58cc6a7ed015d992063a4d294b80eba8a67882
dainshon <81453127+dainshon@users.noreply.github.com>
2024-07-08 02:59:26 +0900

    Test: docker 수정 후 테스트

--- src/main/java/inandout/backend/healthcheck/HealthCheckController.java
+++ src/main/java/inandout/backend/healthcheck/HealthCheckController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
     @GetMapping("/healthcheck")
     public String testConnection() {
-        return "CICD ok";
+        return "Im not ok";
     }
 }
