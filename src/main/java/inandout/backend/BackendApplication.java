3ef1dcdf25c818ae42fe0d3902a4763391f48272
정소민 <yjk70702002@gmail.com>
2024-07-13 02:50:44 +0900

    Feat(#8): log 추가

--- src/main/java/inandout/backend/BackendApplication.java
+++ src/main/java/inandout/backend/BackendApplication.java
@@ -1,14 +1,16 @@
 package inandout.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-		System.out.println("hello inandout");
+		log.info("hello inandout");
 	}
 
 }
