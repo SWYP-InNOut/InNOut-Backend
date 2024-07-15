2d0f96ddb2833e9998509f5ca71eab505c765654
정소민 <yjk70702002@gmail.com>
2024-07-15 14:27:37 +0900

    Feat(#8): DB기반 로그인 검증 로직

---
+++ src/main/java/inandout/backend/service/login/CustumMemberDetailsService.java
@@ -0,0 +1,27 @@
+package inandout.backend.service.login;
+
+import inandout.backend.dto.login.CustomMemberDetails;
+import inandout.backend.entity.member.Member;
+import inandout.backend.repository.login.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustumMemberDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Member> member = memberRepository.findByEmail(email);
+        if (!member.isEmpty()) {
+            // UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomMemberDetails(member.get());
+        }
+        return null;
+    }
+}
