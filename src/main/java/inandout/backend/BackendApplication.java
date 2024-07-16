6157fbb374d827ad5b554d6f41faf7f3e6725dc1
dainshon <shon1107@naver.com>
2024-07-17 00:02:21 +0900

    Chore(#6): CORS 추가

--- src/main/java/inandout/backend/BackendApplication.java
+++ src/main/java/inandout/backend/BackendApplication.java
@@ -3,6 +3,9 @@ package inandout.backend;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Slf4j
 @SpringBootApplication
@@ -13,4 +16,18 @@ public class BackendApplication {
 		log.info("hello inandout");
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// "https://stuffinout.site" 로 들어오는 요청 허용
+				registry.addMapping("/**").allowedOrigins("https://stuffinout.site/")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
