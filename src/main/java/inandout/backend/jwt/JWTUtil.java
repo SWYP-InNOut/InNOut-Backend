5d8dad5bb37cb9afc2a8a3f4cecee18933646013
정소민 <yjk70702002@gmail.com>
2024-07-25 17:30:05 +0900

    Feat(#54): Refresh Token 생성 및 redis에 저장

--- src/main/java/inandout/backend/jwt/JWTUtil.java
+++ src/main/java/inandout/backend/jwt/JWTUtil.java
@@ -1,7 +1,9 @@
 package inandout.backend.jwt;
 
-import io.jsonwebtoken.Jwts;
+import inandout.backend.dto.login.CustomMemberDetails;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -12,6 +14,8 @@ import java.sql.Date;
 @Component
 public class JWTUtil {
     private SecretKey secretKey;
+    private final Long accessTokenValidTime = (60 * 1000L) * 30; // 30분
+    private final Long refreshTokenValidTime = (60 * 1000L) * 60 * 24 * 7; // 7일
 
     public JWTUtil(@Value("${jwt.secret}") String secret) {
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
@@ -22,18 +26,37 @@ public class JWTUtil {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("email", String.class);
     }
 
-    public Boolean isExpired(String token) {
+    public Boolean isExpired(String token) throws ExpiredJwtException{
 
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date(System.currentTimeMillis()));
     }
 
-    public String createJwt(String email, Long expiredMs) {
-
+    public String createAccessToken(String email, Long expiredMs) {
+        Claims claims = Jwts.claims().subject(email).build();
         return Jwts.builder()
-                .claim("email", email)
+                .claims(claims)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + expiredMs))
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public String createRefreshToken(Long expiredMs) {
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public TokenInfo generateToken(String email) {
+        String accessToken = createAccessToken(email, accessTokenValidTime);
+        String refreshToken = createRefreshToken(refreshTokenValidTime);
+
+        return TokenInfo.builder()
+                .grantType("Bearer")
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
     }
 }
---
+++ src/main/java/inandout/backend/jwt/TokenToIpWithId.java
@@ -0,0 +1,17 @@
+package inandout.backend.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Getter
+//@Setter
+//public class TokenToIpWithId implements Serializable {
+//    private String clientIp;
+//    private int memberId;
+//}
