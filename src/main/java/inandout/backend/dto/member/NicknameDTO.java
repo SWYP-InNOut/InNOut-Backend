d0b5283c9109837f3b0ba931f93bac0156e8c518
정소민 <yjk70702002@gmail.com>
2024-07-27 21:21:05 +0900

    Fix(#50): NicknameDTO 수정

--- src/main/java/inandout/backend/dto/member/NicknameDTO.java
+++ src/main/java/inandout/backend/dto/member/NicknameDTO.java
@@ -1,13 +1,10 @@
 package inandout.backend.dto.member;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 public class NicknameDTO {
     private String nickname;
-
-    public NicknameDTO(String nickname) {
-        this.nickname = nickname;
-    }
 }
